345e28a72e8753f791ae4c494499eb408072e542
protapkrghosh <protapg518@gmail.com>
2025-06-22 23:55:04 +0600

    fixed responsive issue

--- src/components/Container.jsx
+++ src/components/Container.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const Container = ({ children }) => {
    return (
-      <div className="max-w-7xl mx-auto px-4 sm:px-2 md:px-4 xl:px-5 2xl:px-8">
+      <div className="max-w-[96rem] mx-auto px-2 sm:px-2 md:px-4 xl:px-5 2xl:px-8">
          {children}
       </div>
    );
